aec8b491650ce70ae3c70d5dfa2fe694d9f90fe8
Pratik <153324296+pratikverse@users.noreply.github.com>
2025-06-12 15:40:56 +0530

    Time: 16 ms (26.75%) | Memory: 17.5 MB (18.78%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,23 +9,14 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode* root, TreeNode* p, std::vector<TreeNode*>& path) {
-    if (!root) return false;
-    path.emplace_back(root);
-    if (root == p) return true;
-    if (helper(root->left, p, path) || helper(root->right, p, path)) {
-        return true;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==nullptr || root==p || root==q){
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p,q);
+        if(left==nullptr) return right;
+        else if (right==nullptr) return left;
+        return root;
     }
-    path.pop_back();
-    return false;
-}
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    vector<TreeNode*> pathP, pathQ;
-    if (!helper(root, p, pathP) || !helper(root, q, pathQ)) return nullptr;
-    int i = 0;
-    while (i < pathP.size() && i < pathQ.size() && pathP[i] == pathQ[i]) {
-        i++;
-    }
-    return pathP[i - 1];
-}
 };
