1c2b74c2f926abbade59277b06f96dfc76ff0185
Pratik <153324296+pratikverse@users.noreply.github.com>
2025-06-30 12:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,48 +1,23 @@
-class Solution
-{
-    public:
-        int search(vector<int> &nums, int target)
-        {
-            int left = 0;
-            int right = nums.size() - 1;
-
-            while (left <= right)
-            {
-                int mid = left + (right - left) / 2;
-
-               	// Check if the middle element is the target
-                if (nums[mid] == target)
-                {
-                    return mid;
-                }
-
-               	// Determine which half of the array is sorted
-                if (nums[left] <= nums[mid])
-                {
-                	// Left half is sorted
-                    if (target >= nums[left] && target < nums[mid])
-                    {
-                        right = mid - 1;
-                    }
-                    else
-                    {
-                        left = mid + 1;
-                    }
-                }
-                else
-                {
-                	// Right half is sorted
-                    if (target > nums[mid] && target <= nums[right])
-                    {
-                        left = mid + 1;
-                    }
-                    else
-                    {
-                        right = mid - 1;
-                    }
-                }
+class Solution {
+public:
+    int binaryHelper(vector<int>&nums, int low, int high, int target){
+        while(low<=high){
+            int mid = (low+high) / 2;
+            if(nums[mid]==target){
+                return mid;
             }
-
-            return -1;	// Target not found
+            if(binaryHelper(nums,mid+1,high, target)!=-1){
+                return binaryHelper(nums,mid+1, high, target);
+            }
+            else{
+                return binaryHelper(nums,low, mid-1,target);
+            } 
+            
+            
         }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        return binaryHelper(nums,0,nums.size()-1,target);
+    }
 };
