b8f65d36e960601a4d0d3ba3db8bdfb71c1fccfd
Pratik <153324296+pratikverse@users.noreply.github.com>
2025-06-30 13:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (12.33%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,20 +1,20 @@
-class Solution
-{
-    public:
-        int findMin(vector<int> &nums)
-        {
-            int l = 0;
-            int r = nums.size() - 1;
-
-            while (l < r)
-            {
-                const int m = (l + r) / 2;
-                if (nums[m] < nums[r])
-                    r = m;
-                else
-                    l = m + 1;
+class Solution {
+public:
+    int findMinHelper(vector<int> nums, int low, int high) {
+        int curLow = INT_MIN;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[low] <= nums[mid]) {
+                curLow = nums[low];
+                return min(curLow, findMinHelper(nums, mid + 1, high));
+            } else {
+                return min(findMinHelper(nums, low, mid-1), findMinHelper(nums, mid, high));
             }
-
-            return nums[l];
         }
+        return INT_MAX; // Added to ensure return in all paths
+    }
+
+    int findMin(vector<int>& nums) {
+        return findMinHelper(nums, 0, nums.size() - 1);
+    }
 };
