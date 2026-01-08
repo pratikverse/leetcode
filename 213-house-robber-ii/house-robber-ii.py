053230441549ce478cf394ceda4f5170a01a6986
Pratik Shrivastava <kpratik653@gmail.com>
2026-01-08 17:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (17.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,20 +1,24 @@
 class Solution(object):
-    def hr1(self, nums):
-            rob1 = 0
-            rob2 = 0
-
-            for i in nums:
-                temp = max(rob1+i, rob2)
-                rob1 = rob2
-                rob2 = temp
-            
-            return rob2
-
     def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        n = len(nums)
+        if n == 0: return 0
+        if n == 1: return nums[0]
+        if n == 2: return max(nums[0], nums[1])
         
+        # Scenario 1: Rob houses 0 to n-2
+        # Scenario 2: Rob houses 1 to n-1
+        return max(self.simple_rob(nums[:-1]), self.simple_rob(nums[1:]))
+            
+    def simple_rob(self, nums):
+        if not nums: return 0
+        n = len(nums)
+        if n == 1: return nums[0]
         
-        return max(nums[0], self.hr1(nums[1:]), self.hr1(nums[:-1]))
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        
+        return dp[-1]
