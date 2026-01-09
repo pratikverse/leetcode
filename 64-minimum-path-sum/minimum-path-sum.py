db6c6a329a2e85c2bb53ca50d3dc5a74c70af98b
Pratik Shrivastava <kpratik653@gmail.com>
2026-01-09 16:44:40 +0530

    Time: 86 ms (5.65%) | Memory: 21.2 MB (5.39%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def cost(self, matrix, i=0, j=0, dp=None):
+        if dp is None:
+            dp = {}
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        if (i, j) in dp:
+            return dp[(i, j)]
+
+        # Base case
+        if i == n - 1 and j == m - 1:
+            dp[(i, j)] = matrix[i][j]
+
+        elif i == n - 1:
+            dp[(i, j)] = matrix[i][j] + self.cost(matrix, i, j + 1, dp)
+
+        elif j == m - 1:
+            dp[(i, j)] = matrix[i][j] + self.cost(matrix, i + 1, j, dp)
+
+        else:
+            dp[(i, j)] = matrix[i][j] + min(
+                self.cost(matrix, i + 1, j, dp),
+                self.cost(matrix, i, j + 1, dp)
+            )
+
+        return dp[(i, j)]
+
+    def minPathSum(self, grid):
+        return self.cost(grid, 0, 0)
